f60bcb6cd33b4882bd2f58196548f7f2a7e92e81
gghhkm <72711094+gghhkm@users.noreply.github.com>
2021-01-06 23:36:52 -0500

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -52,39 +52,11 @@ read main
 
 echo "_________________________________________________________________________________________"
 
-#echo " Now tell me the codename for your device "
-#read code
+echo " Now tell me the codename for your device "
+read code
 
-echo "_________________________________________________________________________________________"
-
-echo " Now lets export some stuff related to Mi A2 [Jasmine_sprout] "
-export OF_MAINTAINER="$main"
-export OF_AB_DEVICE=1
-export ALLOW_MISSING_DEPENDENCIES=true
-export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-export FOX_R11=1
-export FOX_ADVANCED_SECURITY=1
-export FOX_REPLACE_BUSYBOX_PS=1
-export FOX_REPLACE_TOOLBOX_GETPROP=1
-export FOX_USE_TAR_BINARY=1
-export FOX_USE_NANO_EDITOR=1
-export FOX_VERSION="R11 OP6T TEST BUILD"
-export OF_USE_TWRP_SAR_DETECT=1
-export OF_USE_MAGISKBOOT=1
-export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-export OF_NO_SAMSUNG_SPECIAL=1
-export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
-export OF_OTA_BACKUP_STOCK_BOOT_IMAGE=1
-export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
-export OF_PATCH_AVB20=1
-export OF_SUPPORT_PRE_FLASH_SCRIPT=1
-export OF_DONT_KEEP_LOG_HISTORY=1
-export OF_HIDE_NOTCH=1
-export OF_STATUS_INDENT_LEFT=48
-export OF_STATUS_INDENT_RIGHT=48
-export OF_SCREEN_H=2160
-export OF_FBE_METADATA_MOUNT_IGNORE=1
-export OF_CHECK_OVERWRITE_ATTEMPTS=1
+# Import OrangeFox build variables
+source configs/"${code}"_ofconfig
 
 clear
 echo "_________________________________________________________________________________________"
